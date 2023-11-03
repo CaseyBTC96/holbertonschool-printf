5f9f52526b501d5792a893870a284567d77319cf
Casey Paul <7227@holbertonstudents.com>
2023-11-03 13:55:43 -0700

    added functions to main.h

--- main.h
+++ main.h
@@ -2,10 +2,15 @@
 #define _MAIN_H
 #include <stdarg.h>
 #define BUFF_SIZE 1024
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...);
-
+int set_string(va_list valist);
+int _putchar(char c);
+int print_percent(__attribute__((unused))va_list valist);
 
 
 #endif
