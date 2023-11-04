2be7ae794b309e5da8e8b6010fd810f7cf04a16e
Casey Paul <7227@holbertonstudents.com>
2023-11-04 12:03:21 -0700

    completed main.h

--- main.h
+++ main.h
@@ -1,19 +1,32 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef HEADERFILE_H
+#define HEADERFILE_H
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
-#define BUFF_SIZE 1024
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-void print_buffer(char buffer[], int *buff_ind);
+int _printf(const char *format, ...);
+
+/**
+ * struct format - This is a typedef struct.
+ * @opt: Pointer type char.
+ * @call_fun: Pointer to call the function
+ */
+typedef struct format
+{
+	char *opt;
+	int (*call_fun)();
+} format_t;
+
 int _printf(const char *format, ...);
 int set_string(va_list valist);
 int _putchar(char c);
 int print_percent(__attribute__((unused))va_list valist);
-
-
+int parse_format(const char *format, format_t get_opt[], va_list valist;
+int set_char(va_list valist);
+int set_decimal(va_list valist);
+int length_of_integer(int n);
+int get_length(char *str __attribute__((_unused_)), va_list valist);
 #endif
